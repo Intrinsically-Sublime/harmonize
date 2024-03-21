ff592287204f03dc5906eb24626199b669490e05
Sublime <amailbox@zohomail.com>
2024-03-20 22:50:06 -0700

    Update decoders.py
    Add MP4 as source

--- harmonize/decoders.py
+++ harmonize/decoders.py
@@ -41,7 +41,34 @@ async def flac(path):
 async def mp3(path):
     """Decode a MP3 file
 
-    :param pathlib.Path path: The FLAC file path
+    :param pathlib.Path path: The MP3 file path
+    """
+    read_pipe, write_pipe = os.pipe()
+
+    proc = await asyncio.create_subprocess_exec(
+        'ffmpeg', '-i', path, '-f', 'wav', '-',
+        stdout=write_pipe,
+        stderr=asyncio.subprocess.PIPE)
+
+    os.close(write_pipe)
+
+    yield read_pipe
+    await proc.wait()
+    # Decode errors may are non-fatal, but may indicate a problem
+    stderr = await proc.stderr.read()
+    if proc.returncode:
+        raise asyncio.subprocess.CalledProcessError(
+            proc.returncode,
+            proc.args,
+            stderr=stderr
+        )
+
+
+@contextlib.asynccontextmanager
+async def mp4(path):
+    """Decode a MP4 file
+
+    :param pathlib.Path path: The MP4 file path
     """
     read_pipe, write_pipe = os.pipe()
 
